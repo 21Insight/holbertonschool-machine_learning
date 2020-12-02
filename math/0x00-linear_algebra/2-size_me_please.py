782b0d0e8f9a286a682c98547461d79e94a1aa15
Jose Cuervo <manuelmuoz1221@outlook.com>
2020-12-02 03:56:53 -0500

    fix: comments

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import numpy as np
+"""A function def matrix_shape(matrix): that calculates the shape of a matrix"""
 
 
 def matrix_shape(matrix):
+    """Funtion shape of numpy"""
     shape = list(np.shape(matrix))
     return shape
