43e81dd71c66ff7cf193856547c1c3ea1576d468
Jose Cuervo <manuelmuoz1221@outlook.com>
2020-12-02 04:04:04 -0500

    feature: Transpose of a 2D matrix

---
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Returns the transpose of a 2D matrix """
+
+
+def matrix_transpose(matrix):
+    """Calcuates the transpose of a matrix"""
+    range_ax0 = range(len(matrix[0]))
+    range_ax1 = range(len(matrix))
+
+    return [[matrix[j][i] for j in range_ax1] for i in range_ax0]
