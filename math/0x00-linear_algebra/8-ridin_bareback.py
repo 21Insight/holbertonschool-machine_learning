75c3ed1b45ea7ac41771bbd5cad32500dacc7b81
Jose Cuervo <manuelmuoz1221@outlook.com>
2020-12-03 15:56:06 -0500

    feature: Multiplicates two 2D matrices

---
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Performs matrix multiplication"""
+
+
+def mat_mul(mat1, mat2):
+    """Multiplicates two 2D matrices"""
+    if len(mat1[0]) != len(mat2):
+        return None
+
+    range1 = range(len(mat1))
+    range2 = range(len(mat2[0]))
+    rax = range(len(mat1[0]))
+
+    result = [[sum([mat1[i][k] * mat2[k][j] for k in rax])
+               for j in range2] for i in range1]
+    return result
