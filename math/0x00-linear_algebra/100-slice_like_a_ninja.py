4db6ba00c072b761cdbed0a7a1ad7447181d5c32
Jose Cuervo <manuelmuoz1221@outlook.com>
2020-12-03 23:13:23 -0500

    feature: slices a matrix along a specific axes

---
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Slices a matrix along a specific axes"""
+
+
+def np_slice(matrix, axes={}):
+    """Slices a passed matrix with some specified axis"""
+    hight_axis = max(axes, key=int) + 1
+    slice_obj = tuple([slice(*axes.get(i) or (None,))
+                       for i in range(hight_axis)])
+    return matrix[slice_obj]
