42dcf563edfee0d7a74a54c946474849a4f11307
Jose Cuervo <manuelmuoz1221@outlook.com>
2021-02-25 09:27:53 -0500

    feat: 0-yolo.py

---
+++ supervised_learning/0x0A-object_detection/0-yolo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""class Yolo | Perform object detection"""
+
+import tensorflow.keras as K
+
+
+class Yolo:
+    """Yolo Class"""
+
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """class constructor"""
+        with open(classes_path) as f:
+            classes_t = f.readlines()
+        classes = [x.strip() for x in classes_t]
+        self.model = K.models.load_model(model_path)
+        self.class_names = classes
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
